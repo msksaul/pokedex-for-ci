5efc31769c9a31c7c1d0bcb4d54f8281c933ff30
mscalel <msksaul@gmail.com>
2022-07-26 16:23:31 -0600

    Heroku Deploy with Secrets

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
+const { version } = require('./package.json')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (_req, res) => {
+  res.send(version)
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server started on port 5000')
+  console.log(`Server started on port ${PORT}`)
 })
